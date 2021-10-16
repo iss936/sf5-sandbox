8bf2597e603f4217cbada1966ea8d223573d2c8d
iss936 <soumare.iss@gmail.com>
2021-10-16 15:52:57 +0200

    Create SystemController.php
    add some example with filesystem

---
+++ src/Controller/SystemController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/system/filesystem", name="app_system_filesystem")
+     * @param Security $security
+     * @return Response
+     */
+    public function index(): Response
+    {
+    	$em = $this->getDoctrine()->getManager();
+        $fs = new Filesystem();
+        try {
+//            $fs->mkdir(['/var/www/html/current/issasama', '/var/www/html/current/kakarot'], 0777);
+//            $exist = $fs->exists(['/var/www/html/current/issasama', '/var/www/html/current/kakarots']);
+//            $exist = $fs->copy('/var/www/html/current/issa.txt', '/var/www/html/current/idr.txt', true);
+//            $targetLink = $fs->readLink('/var/www/html/current/link-sym.txt', true);
+            $fs->appendToFile('/var/www/html/current/kak.txt', 'Hello World');
+//            dump($targetLink);
+            die('ok');
+        } catch (IOExceptionInterface $exception) {
+            echo "An error occurred while creating your directory at ".$exception->getPath();
+        }
+    }
+}
